2f66bf07edf26d2a2bc575759d92eeff89b52002
CBerger1997 <callumBerger@hotmail.com>
2023-03-30 00:45:22 +0100

    Algorithm Test Files Added

---
+++ AiM Algorithm Test/AiM Algorithm Test/Scene.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AiM_Algorithm_Test
+{
+    internal class Scene
+    {
+        bool isStart;
+        bool isEnd;
+        int[] futureConnections;
+        int[] pastConnections;
+        float weight;
+    }
+}
