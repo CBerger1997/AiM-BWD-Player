46a00dc449b0448db892ef1e9b7f8e55bc47d07f
CBerger1997 <callumBerger@hotmail.com>
2023-03-31 01:24:01 +0100

    Algorithm Implementation
    Added setup of scene class and started algorithm implementation

--- AiM Algorithm Test/AiM Algorithm Test/Program.cs
+++ AiM Algorithm Test/AiM Algorithm Test/Program.cs
@@ -1,7 +1,73 @@
-﻿internal class Program
-{
-    private static void Main(string[] args)
-    {
-        Console.WriteLine("Hello, World!");
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+internal class Program {
+    List<AiM_Algorithm_Test.Scene> scenes = new List<AiM_Algorithm_Test.Scene> ();
+
+    List<AiM_Algorithm_Test.Scene> currenSceneOrder = new List<AiM_Algorithm_Test.Scene> ();
+    List<List<AiM_Algorithm_Test.Scene>> previousSceneOrders = new List<List<AiM_Algorithm_Test.Scene>> ();
+
+    bool isValenceHigher;
+    bool isArousalHigher;
+    
+    int sceneCount;
+    int numOfScreenings;
+
+    private static void Main ( string[] args ) {
+        Console.WriteLine ( "Hello, World!" );
+
+        Program program = new Program ();
+
+        //Define the setup for each scene 1 - 10
+        program.SceneSetup ();
+
+        //Set the higher and lower values for valence and arousal
+        program.SetValenceArousalValues ();
+
+        //Configure the amount of screenings and the total current scene count
+        program.ConfigureScreeningsAndSceneCount ();
+
+        //Assign weights to scenes based on the higher and lower values for valence and arousal
+        program.ModifyWeightsForValenceAndArousal ();
+
+        //Create the scene order
+        program.CreateSceneOrder ();
+    }
+
+    private void SceneSetup () {
+        for ( int i = 0; i < 10; i++ ) {
+            AiM_Algorithm_Test.Scene newScene = new AiM_Algorithm_Test.Scene ( i + 1 );
+            scenes.Add ( newScene );
+        }
+    }
+
+    private void SetValenceArousalValues () {
+        isValenceHigher = Convert.ToBoolean ( new Random ().Next ( 2 ) );
+        isArousalHigher = Convert.ToBoolean ( new Random ().Next ( 2 ) );
+    }
+
+    private void ConfigureScreeningsAndSceneCount () {
+        /*
+         * Here we want to configure how many screenings there are,
+         * then define the scene count range,
+         * and finally define the actual scene count for this screening
+         */
+    }
+
+    private void ModifyWeightsForValenceAndArousal () {
+        /*
+         * Here we want to add directly to the weight values based on arousal responsibility graph and valence positivity graph
+         */
+    }
+
+    private void CreateSceneOrder () {
+        /*
+         * Here we want to create a scene order, 
+         * checking for first scene to apply the start weight,
+         * then ignore start weight and go off current weight and future links,
+         * then include end weight for final scene
+         * 
+         * Later apply previous scene orders as additional weightings for current scene order
+         */
     }
 }
--- AiM Algorithm Test/AiM Algorithm Test/Scene.cs
+++ AiM Algorithm Test/AiM Algorithm Test/Scene.cs
@@ -4,14 +4,61 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace AiM_Algorithm_Test
-{
-    internal class Scene
-    {
-        bool isStart;
-        bool isEnd;
-        int[] futureConnections;
-        int[] pastConnections;
+namespace AiM_Algorithm_Test {
+    internal class Scene {
+        int startWeighting;
+        int endWeighting;
+        int futureLink;
+        int pastConnection;
         float weight;
+
+        public Scene () {
+            startWeighting = 0;
+            endWeighting = 0;
+            futureLink = 0;
+            pastConnection = 0;
+            weight = 0.0f;
+        }
+
+        public Scene ( int sceneID ) {
+            switch ( sceneID ) {
+                case 1:
+                    startWeighting = 100;
+                    futureLink = 6;
+                    break;
+                case 2:
+                    startWeighting = 100;
+                    endWeighting = 80;
+                    futureLink = 7;
+                    break;
+                case 3:
+                    startWeighting = 80;
+                    futureLink = 8;
+                    break;
+                case 4:
+                    startWeighting = 60;
+                    endWeighting = 80;
+                    futureLink = 9;
+                    break;
+                case 5:
+                    endWeighting = 100;
+                    futureLink = 10;
+                    break;
+                case 6:
+                    startWeighting = 80;
+                    break;
+                case 7:
+                    startWeighting = 100;
+                    endWeighting = 100;
+                    break;
+                case 8:
+                    startWeighting = 80;
+                    break;
+                case 10:
+                    startWeighting = 100;
+                    endWeighting = 100;
+                    break;
+            }
+        }
     }
 }
